e01677e413319aa51ad8cac07cb4278e36315b76
Ziva Sacharissa <zivadanika@gmail.com>
2022-09-27 10:50:18 +0100

    Added Mars Feature

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
-print("hello world")
-print("second commit")
+import sys
 
+der hello():
+	if sys.argv[1] == "Mars":
+		hellomars()
+	else:
+		helloworld()
+
+def hellomars():
+	print("Hello world")
+
+def helloworld():
+	print("Hello world")
